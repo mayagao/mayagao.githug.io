eec2d4aaea637f11e5892f7af4d4c2a273e738b1
mayagao <mayaqgao@gmail.com>
2017-06-12 14:05:03 -0400

    try

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+// next.config.js
 module.exports = {
-  webpack: (config, { dev }) => {
-    config.module.rules.push({ test: /\.css$/, loader: 'css-loader' });
-    return config;
-  }
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" }
+    }
+  },
 }
