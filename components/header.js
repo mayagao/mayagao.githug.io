46e6953bcd8983019e933f60817763c1cee6cb1e
mayagao <mayaqgao@gmail.com>
2017-06-12 13:45:46 -0400

    🍇

---
+++ components/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from 'next/head'
+export default () => (
+    <Head>
+      <meta charset="utf-8" />
+      <title>Maya Gao</title>
+      <meta name="description" content="" />
+      <meta name="author" content="" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5, minimum-scale=.25, user-scalable=yes"/>
+      <link rel="stylesheet" href="https://unpkg.com/tachyons@4.6.1/css/tachyons.min.css"/>
+      <link rel="stylesheet" href="static/site.css"/>
+    </Head>
+)
---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  webpack: (config, { dev }) => {
+    config.module.rules.push({ test: /\.css$/, loader: 'css-loader' });
+    return config;
+  }
+}
