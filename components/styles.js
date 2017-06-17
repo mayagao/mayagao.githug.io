0d05a59a66223b4620d0b20e58042c3c8242b611
mayagao <mayaqgao@gmail.com>
2017-06-17 19:08:32 -0400

    style editor copy polish

--- components/styles.js
+++ components/styles.js
@@ -8,11 +8,13 @@ const styles = {
   rightColumnContent: 'absolute-ns relative mt3 mt0-ns bottom-0',
   // typography
   txtH1:'f1 headline mb2',
-  txtH2: 'f3 o-80 mb3',
+  txtH2: 'f2 headline o-80 mb3',
   txtH3: 'f5 mb1',
   txtBody: 'lh-copy f5 o-70',
   txtSbody: 'lh-copy f6 f5 o-70',
-  txtNote: 'o-50 mt3 pl1 f7 lh-copy'
+  txtNote: 'o-50 mt3 pl1 f7 lh-copy',
+  //
+  quote: 'quote b--black-30 lh-copy pl3 bl f4 o-60 mt4 mb4'
 };
 
 export { styles };
--- pages/mapbox-studio.js
+++ pages/mapbox-studio.js
@@ -47,34 +47,34 @@ const featureDetails = [
   {
     imageSource: '../static/img/ms_3.png',
     title: 'Different layer states',
-    description: 'Clear, distinguishable states for nested, selected, and hidden layers.'
+    description: 'Clear, distinguishable states for nested, selected and hidden layers.'
   },
   {
     imageSource: '../static/img/ms_1.png',
     title: 'Property categories',
-    description: 'Improve clarity by dividing layer properties into four different categories.'
+    description: 'Divid layer properties into four categories to improve visibility.'
   },
   {
     imageSource: '../static/img/ms_2.png',
-    title: 'Data selection popover',
-    description: 'Visual hierarchy between the parent data sources and their subset sources.'
+    title: 'Data source hierarchy',
+    description: 'Visual hierarchy between the parent sources and their subset sources.'
   }
 ]
 
 const functionDetails = [
   {
     imageSource: '../static/img/ramp_1.png',
-    title: 'Function graph',
+    title: `Ramp graphs`,
     description: 'The large detailed graphs helps users understand how functions work and how value change at various zoom levels. I ended up simplifying the small graphs a lot more to make them look less clickable.'
   },
   {
     imageSource: '../static/img/ms_ramp_edit.png',
     title: 'Editable graph',
-    description: `Deciding whether to build something is always hard. Prototyping helped us discover some accuracy intricacies with this interaction. So we didn't move forward with it.`
+    description: `Prototyping helped us discover some accuracy intricacies with this interaction. So we didn't move forward with it.`
   },
   {
     imageSource: '../static/img/ramp_2.png',
-    title: 'Position indicator and distinct styles',
+    title: 'Position indicator',
     description: 'I added a diamond pointer to indicate values at current zoom level. The distinct styles of small graphs help users differentiate between color, enum and number functions at a glance. '
   }
 ]
@@ -92,26 +92,23 @@ export default () => (
 
   <div className={`${styles.limiter} ${styles.columnContainer}`}>
     <div className={`${styles.leftColumn}`}>
-      <a href="" className="db mb4 relative link light-silver hover-moon-gray">
+      <a href="https://www.wired.com/2015/09/mapbox-studio/" className="db mb4 relative link light-silver hover-moon-gray">
         <div>
           <img style={{width: 100}} className="o-20" src="../static/img/wired.svg" />
           <div className="quote f4 mt2">A Powerful New Mapmaking Tool Fit for Both Pros and Newbies</div>
         </div>
       </a>
-      <div className={`${styles.txtBody} mb5`}>
+      <div className={`${styles.txtBody} mb4`}>
         I started working on Mapbox Studio four months before its public launch.
-        A main focus of the release was the style editor,
-        a professional map design tool. With Studio users can select a map layer,
-        edit data sources of the layer, and then define certain styling properties such as font-size and opacity.
+        A main focus of the release was the style editor, a professional map design tool.
         As the first visual designer in the team, I was tasked to create a more cohesive,
         consistent and friendly visual language for the tool.
       </div>
       <div className={`${styles.txtH2}`}>Style Editor</div>
       <div className={`${styles.txtBody}`}>
-        Before Mapbox Studio style editor,
-        there wasn't really a digital map design tool availalbe that provide complete design control for things like texture, color and fonts.
+        The editor provides complete control over how the map looks. Users can select a map layer,
+        edit data sources, and then modify properties such as texture, color and fonts.
         Retaining flexibility that comes with all those poweful customization options,
-        providing visibility for the important features,
         while at the same time designing a UI that feels approachable and learnable was definitely a big challenge.
       </div>
     </div>
@@ -124,22 +121,21 @@ export default () => (
       </div>
   </div>
 
-  <div className="w-100 mt4 bg-mb pv3-l p3 pl4-l pl3 pr2">
+  <div className="w-100 mt4 bg-mb pv3-l p3 pl4-l pl3 pr2 ">
    <div className="pt4 white f6 flex">
      <div style={{width: 1200}} className=" self-center center mb0">
-       <div ><span className="o-70">Style Editor Iteration</span> <span className="o-40">August, 2015</span></div>
+       <div ><span className="o-70">Style Editor Iteration</span></div>
        <div style={{zIndex: 11111}} className="w-100 none f7 db-l dn nb3 o-50 mt3 cf">
          {[`Layer list`, `Data source panel`,`Data source popover`, `Layer editing panel`, `Property panel`].map((p, i) => (
            <div key={i} className="w-20 fl relative">{p}<hr className="vertical h1 nb1 db absolute ml2 o-70"/></div> ))}
        </div>
      </div>
     </div>
-    <div className="flex flex-row pb2">
+    <div className="flex flex-row pb2 overflow-hiddden">
       <img style={{width: 1200}} className="center  db-ns dn self-center" src="../static/img/ms_editor.png" />
       <img style={{maxWidth: 230+'%'}} className="center  db dn-ns self-center" src="../static/img/ms_editor.png" />
     </div>
   </div>
-
 
 
 <div className="bg-near-white pv3 lh-copy">
@@ -159,18 +155,36 @@ export default () => (
     <div className={`${styles.columnContainer}`}>
       <div className={`${styles.leftColumn}`}>
         <div className={`${styles.txtH2}`}>Property Function</div>
-        <div className={`${styles.txtBody}`}>Property functions provides allows for adjusting values at different <b>zoom stops</b>. There are a few complexities that comes with the property function: </div>
-        <ol className={`${styles.txtBody} pv3 mb-list`}>
-          <li>Functions of number, color and enum properties behave in different ways. </li>
-          <li>For number properties, users can also control how fast the value increase/decreases with zoom through changing the exponential base of the interpolation curve. </li>
-          <li>There are certain validation rules for functions, for example, zoom stops must always be in ascending order with no duplicates. </li>
-        </ol>
-        <div className={`${styles.txtBody}`}>To help users better understand how property function works, we introduced detailed ramp graphs, one small graph next to each property entry and another detailed graph inside each function editing popover. We also added automatic sorting and other smart validation rules to prevent people from making mistakes.</div>
+        <div className={`${styles.txtBody} mb3`}>
+          Property functions allows for adjusting values at different <b>zoom stops</b>.
+          There are a few complexities that comes with desigining this.
+          Functions of number, color and enum properties behave in different ways.
+          For number properties, users can control how fast the value increase/
+          decreases through changing the exponential base of the curve.
+          Functions need to meet certain requirements to be valid.
+        </div>
+        <div className={`${styles.quote}`}>
+          Priotization and deciding on what to build is always hard.
+          User testing and prototyping helped us better identify problems and manage implementation scopes.
+        </div>
+
+        <div className={`${styles.txtBody}`}>
+          To help users better understand how property function works,
+          we introduced detailed ramp graphs to indicate precisely how values change with zoom level.
+          We also added automatic sorting and other smart validation rules to prevent people{` `}
+          from making mistakes. We abandoned many seemingly great ideas along the way,
+          such as making an editable graph. Priotization and deciding on what to build is always hard.
+          User testing and prototyping helped us better identify problems and manage implementation scopes.
+        </div>
+
       </div>
       <div className={`${styles.rightColumn}`}>
         <div  className={`${styles.rightColumnContent}`}>
           <img style={{width: 154}} className="nl2 dib" src="../static/img/ramp.png" />
-          <div className={`${styles.txtNote}`}>Increase the city label size when zooming in.</div>
+          <div className={`${styles.txtNote}`}>
+            An example use case of propoerty function:
+            increase the city label size when zooming in.
+          </div>
         </div>
       </div>
     </div>
@@ -194,27 +208,31 @@ export default () => (
   <div className={`${styles.limiter}`}>
     <div className={`${styles.leftColumn} mb4 mt4`}>
       <div className={`${styles.txtH2}`}>Dashboard</div>
-      <div className={`${styles.txtBody}`}>In addition to map design, Studio also serves as an online platform that connects all the Mapbox products, from the data upload and processing pipeline to the developer APIs and SDKs. Users can design maps, upload and manage custom data, analyze API usage and manage account, all in one place.</div>
+      <div className={`${styles.txtBody}`}>
+        In addition to being a map design tool, Studio also serves as an online platform that connects all the different Mapbox products,
+        from the data upload and processing pipeline to the developer APIs and SDKs. Users can design maps,
+        upload and manage custom data, analyze API usage and manage account, all in one place.
+      </div>
     </div>
     <div className="w-100 mt5 flex-l flex-none br2 mb3">
-      <div style={{flex: 'auto 1 824px'}} className="br2">
+      <div style={{flex: 'auto 1 824px'}} className="">
          <img style={{width: 824}} className="" src="../static/img/slide1.png" />
       </div>
       <div  style={{flex: '1 1 240px'}} className="lh-copy flex f7 pl4-l pl0">
         <div className="self-end mb4-l">
           <div className={`${styles.txtH3}`}>Home page</div>
-          <div className={`${styles.txtSbody} mt2`}>The goal of the home page is to help user navigate and find what they are looking for. We identified three most common tasks: design, develop and data visualization and made them easy accessible from the main view. </div>
+          <div className={`${styles.txtSbody} mt2`}>The goal of the home page is to help user navigate and find what they are looking for. We identified three most common tasks: design, develop and data visualization. </div>
         </div>
       </div>
     </div>
     <div className="w-100 mt4 flex-l flex-none br2 mb4">
-      <div style={{flex: `auto 1 824px`}}  className="br2">
+      <div style={{flex: `auto 1 824px`}}  className="">
          <img style={{width: 824}} className="" src="../static/img/slide2.png" />
       </div>
       <div style={{flex: `1 1 240px`}} className="lh-copy flex pl4-l pl0">
         <div className="self-end f7 mb4-l">
           <div className={`${styles.txtH3}`}>Listing pages</div>
-          <div className={`${styles.txtSbody} mt2`}>The listing pages consist of a left main view with all styles/tilesets in a user's account, and a right sidebar explaining how to use those resources. There is also a data upload popover showing the status of current upload. </div>
+          <div className={`${styles.txtSbody} mt2`}>The listing pages consist of a left main view with all styles/tilesets in a user's account, and a right sidebar explaining how to use those resources. The left popover shows status of current data uploads. </div>
         </div>
       </div>
     </div>
@@ -222,24 +240,22 @@ export default () => (
 
   <div className="w-100 bg-near-white pv4 lh-copy">
     <div className={`${styles.limiter}`}>
-      <div className={`${styles.txtH2}`}>Modal</div>
-      <div className={`${styles.txtBody} mb4`}>Modal is another main UI component. The challenge here is explaining abstract concepts and displaying large amount of information in a very confined space.</div>
+      <div className={`${styles.txtBody} mb4`}>Modal is another main UI component. The challenge here is often explaining abstract concepts or displaying large amount of information in a confined space.</div>
       <div className="flex-l flex-none">
         <div style={{flex: `1 1 501px`}} className="mr4-l mb4 mb0-l mr0">
           <div className=" overflow-y-hidden mb2">
-             <img style={{width: 501}} className="" src="../static/img/ms_modal_create_style.png" />
+             <img style={{width: 501}} className="shadow" src="../static/img/ms_modal_create_style.png" />
           </div>
           <div className={`${styles.txtH3}`}>New style modal</div>
           <div  className={`${styles.txtSbody} mw6`}>There are six styles users can choose as a base template for their design. The goal of this modal is to help them make an informed decision on which style to pick based on different needs. </div>
         </div>
         <div style={{flex: `1 1 397px`}}  className="ml4-l ml0">
           <div className="overflow-y-hidden mb2">
-             <img style={{width: 397}} className="" src="../static/img/ms_modal_composite.png" />
+             <img style={{width: 397}} className="shadow" src="../static/img/ms_modal_composite.png" />
           </div>
           <div className={`${styles.txtH3}`}>Layer composite modal</div>
-          <div  className={`${styles.txtSbody} mw6`}>Composite means combining various data sources for use cases like label collision detection. Here I tried to use illustration to explain a abstract concept. </div>
+          <div  className={`${styles.txtSbody} mw6`}>Composite means combining various data sources for use cases like label collision detection. Here I tried to use illustration to explain the concept a bit better. </div>
         </div>
-
       </div>
     </div>
   </div>
@@ -250,14 +266,31 @@ export default () => (
       <div className={`${styles.leftColumn}`}>
         <div className={`${styles.txtH2}`}>What I learned</div>
         <div className={`${styles.txtBody} mb3`}>
-          Coming to the team at a later stage definitely makes the job more daunting since I have less context about the product history. Luckily, the team at Mapbox values deeply about open communication and they documented everything, down to every small UI tweaks, on Github. A big part of what I do at the beginning was just reading through the ticket to better understand how certain decisions are made.</div>
-         <div className={`${styles.txtBody} mb3`}>
-          This experience helped me understand the importance of documentation and communication. I tried my best later on to apply the same principles to other projects I worked on: 1) Demonstrate clearly with mockups what I am trying to achieve, 2) Gather constant feedbacks from engineers to build consensus, and 3) Document the reason behind every design decision with clarity.  </div>
-         <div className={`${styles.txtBody}`}>
-          Using the same tool to design and develop and communicate really helps build empathy between designers and engineers. In a small team and a highly iterative environment, opening a pull request also brings more credibility to the design decisions than ../static images. It allows me to establish trust and introduce systematic changes merely a few months before the launch. </div>
+          Coming to the team at a later stage definitely makes the job more challenging since I have less context about the product history.
+          A big part of what I do at the beginning was just reading through the Github ticket to better understand how decisions are made,
+          since everything, down to every small UI tweaks, are documentated on Github.
+        </div>
+        <div className={`${styles.quote}`}>
+          Using the same tool helps build deeper{` `}
+          empathy between designers and engineers.
+          Prototyping in code brings more credibility to my design decisions.
+        </div>
+        <div className={`${styles.txtBody} mb3`}>
+          Open communication becomes especially important when collaborating closely with a enginnering team,
+          I've learned to always:
+          1) Demonstrate clearly with mockups and prototypes what I am trying to achieve,
+          2) Gather constant feedbacks from engineers to build consensus,
+          and 3) Document the reason behind every design decision with clarity.
+        </div>
+
+        <div className={`${styles.txtBody}`}>
+          Using the same tool (Github) to design and develop and communicate really helps build deeper{` `}
+          empathy between designers and engineers. In a highly iterative environment,
+          opening a pull request also brings more credibility to the design decisions than static images.
+          It allows me to establish trust and introduce systematic changes merely a few months before the launch.
+        </div>
       </div>
     </div>
-
     <div className="flex-l flex-none">
       <div  style={{background: `#3887BE`}} className="flex-auto br3 mr2-l mr0 justify-center flex items-center pv5 ph4">
         <img style={{width: 360}} src="../static/img/2.svg" />
@@ -267,11 +300,7 @@ export default () => (
       </div>
     </div>
      <div className="o-60 f7 mt3">Some illustrations I did for the Studio sign up page. </div>
-
   </div>
-
-
-
   <WorkFooter />
 </Page>)
 
