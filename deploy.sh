6e36810ca29785d0553aca42b698c324e213a86d
Maya Gao <mayaqgao@gmail.com>
2018-06-10 00:19:01 -0700

    fix script

--- deploy.sh
+++ deploy.sh
@@ -2,21 +2,18 @@
 
 STATUS="$(git status)"
 
-if [[ $STATUS == *"nothing to commit, working directory clean"* ]]
-then
-    touch out/.nojekyll
-    touch out/CNAME
-    echo 'mayagao.com' >> out/CNAME
-    git add .
-    git commit -m "Edit .gitignore to publish"
-    git checkout -b release
-    git push origin -f release
-    git push origin `git subtree split --prefix out release`:gh-pages --force
-    git reset --soft HEAD~1
-    git checkout .gitignore
-    git checkout master
-    git reset --hard origin/master
-    echo "💎 Deploy success!"
-else
-    echo "Need clean working directory to deploy"
-fi
+git branch -D release
+
+touch out/.nojekyll
+touch out/CNAME
+echo 'mayagao.com' >> out/CNAME
+git add .
+git commit -m "Edit .gitignore to publish"
+git checkout -b release
+git push origin -f release
+git push origin `git subtree split --prefix out release`:gh-pages --force
+git reset --soft HEAD~1
+git checkout .gitignore
+git checkout master
+git reset --hard origin/master
+echo "💎 Deploy success!"
