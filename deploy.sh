2a095a33f624ce40f8260bf51f22dbf12a0e5e55
Maya Gao <mayaqgao@gmail.com>
2018-06-10 00:05:17 -0700

    test deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,18 @@ STATUS="$(git status)"
 
 if [[ $STATUS == *"nothing to commit, working directory clean"* ]]
 then
-    sed -i "" 'out/' ./.gitignore
     touch out/.nojekyll
     touch out/CNAME
     echo 'mayagao.com' >> out/CNAME
     git add .
     git commit -m "Edit .gitignore to publish"
+    git checkout -b release
+    git push origin -f release
     git push origin `git subtree split --prefix out release`:gh-pages --force
     git reset --soft HEAD~1
     git checkout .gitignore
+    git checkout master
+    git reset --hard origin/master
     echo "💎 Deploy success!"
 else
     echo "Need clean working directory to deploy"
