a35e85519b530bfb7528a5fea075e4720d5df656
Maya Gao <mayagao@users.noreply.github.com>
2019-02-15 15:55:52 -0800

    Add rebrand project (#27)

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ module.exports = {
       "/data-editor": { page: "/data-editor" },
       "/navigation-sdk": { page: "/navigation-sdk" },
       "/data-driven-styling": { page: "/data-driven-styling" },
+      "/runkit-brand": { page: "/runkit-brand" },
       "/about": { page: "/about" }
     };
   }
