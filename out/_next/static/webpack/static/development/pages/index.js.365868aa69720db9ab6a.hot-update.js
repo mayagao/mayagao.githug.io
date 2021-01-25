94bb3fe2df6e757a98dd452a8d787fe7f5e6b2d6
Maya Gao <mayaqgao@gmail.com>
2021-01-24 21:08:54 -0800

    Edit .gitignore to publish

---
+++ out/_next/static/webpack/static/development/pages/index.js.365868aa69720db9ab6a.hot-update.js
@@ -0,0 +1,419 @@
+webpackHotUpdate('static/development/pages/index.js', {
+  /***/ './pages/index.js':
+    /*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+    /*! exports provided: default */
+    /***/ function (module, __webpack_exports__, __webpack_require__) {
+      'use strict'
+      __webpack_require__.r(__webpack_exports__)
+      /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+        /*! react */ './node_modules/react/index.js',
+      )
+      /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/ __webpack_require__.n(
+        react__WEBPACK_IMPORTED_MODULE_0__,
+      )
+      /* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+        /*! next/link */ './node_modules/next/link.js',
+      )
+      /* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/ __webpack_require__.n(
+        next_link__WEBPACK_IMPORTED_MODULE_1__,
+      )
+      /* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(
+        /*! next/head */ './node_modules/next/dist/next-server/lib/head.js',
+      )
+      /* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/ __webpack_require__.n(
+        next_head__WEBPACK_IMPORTED_MODULE_2__,
+      )
+      /* harmony import */ var _components_styles__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(
+        /*! ../components/styles */ './components/styles.js',
+      )
+      /* harmony import */ var _components_header__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(
+        /*! ../components/header */ './components/header.js',
+      )
+      var _jsxFileName = '/Users/interviewing/dev/mayagao.com/pages/index.js'
+      var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement
+
+      var InternetLinks = function InternetLinks() {
+        return __jsx(
+          'div',
+          {
+            className: 'internet-links',
+            __source: {
+              fileName: _jsxFileName,
+              lineNumber: 8,
+            },
+            __self: this,
+          },
+          __jsx(
+            'a',
+            {
+              key: 0,
+              href: 'https://dribbble.com/mayagao',
+              target: '_blank',
+              __source: {
+                fileName: _jsxFileName,
+                lineNumber: 9,
+              },
+              __self: this,
+            },
+            'Dribbble',
+          ),
+          __jsx(
+            'a',
+            {
+              href: 'https://www.linkedin.com/in/mayagq/',
+              target: '_blank',
+              __source: {
+                fileName: _jsxFileName,
+                lineNumber: 12,
+              },
+              __self: this,
+            },
+            'LinkedIn',
+          ),
+          __jsx(
+            'a',
+            {
+              key: 2,
+              href: 'https://www.figma.com/@mayagao',
+              target: '_blank',
+              __source: {
+                fileName: _jsxFileName,
+                lineNumber: 15,
+              },
+              __self: this,
+            },
+            'Figma',
+          ),
+          __jsx(
+            'a',
+            {
+              key: 3,
+              href: 'https://twitter.com/mayaqgao',
+              target: '_blank',
+              __source: {
+                fileName: _jsxFileName,
+                lineNumber: 18,
+              },
+              __self: this,
+            },
+            'Twitter',
+          ),
+          __jsx(
+            'a',
+            {
+              key: 4,
+              href: 'mailto:mayaqgao@gmail.com',
+              target: '_blank',
+              __source: {
+                fileName: _jsxFileName,
+                lineNumber: 21,
+              },
+              __self: this,
+            },
+            'Email',
+          ),
+        )
+      }
+
+      /* harmony default export */ __webpack_exports__['default'] = function () {
+        return __jsx(
+          'div',
+          {
+            className: '',
+            __source: {
+              fileName: _jsxFileName,
+              lineNumber: 27,
+            },
+            __self: this,
+          },
+          __jsx(
+            next_head__WEBPACK_IMPORTED_MODULE_2___default.a,
+            {
+              __source: {
+                fileName: _jsxFileName,
+                lineNumber: 28,
+              },
+              __self: this,
+            },
+            __jsx(
+              'style',
+              {
+                __source: {
+                  fileName: _jsxFileName,
+                  lineNumber: 29,
+                },
+                __self: this,
+              },
+              '\n\n    ',
+            ),
+          ),
+          __jsx(_components_header__WEBPACK_IMPORTED_MODULE_4__['default'], {
+            __source: {
+              fileName: _jsxFileName,
+              lineNumber: 33,
+            },
+            __self: this,
+          }),
+          __jsx(
+            'div',
+            {
+              id: 'new-home',
+              __source: {
+                fileName: _jsxFileName,
+                lineNumber: 34,
+              },
+              __self: this,
+            },
+            __jsx(
+              'div',
+              {
+                id: 'grid-container',
+                __source: {
+                  fileName: _jsxFileName,
+                  lineNumber: 35,
+                },
+                __self: this,
+              },
+              __jsx(
+                'div',
+                {
+                  id: 'first',
+                  className: 'column',
+                  __source: {
+                    fileName: _jsxFileName,
+                    lineNumber: 36,
+                  },
+                  __self: this,
+                },
+                __jsx(
+                  'div',
+                  {
+                    __source: {
+                      fileName: _jsxFileName,
+                      lineNumber: 37,
+                    },
+                    __self: this,
+                  },
+                  __jsx(
+                    'div',
+                    {
+                      className: 'subheader',
+                      __source: {
+                        fileName: _jsxFileName,
+                        lineNumber: 38,
+                      },
+                      __self: this,
+                    },
+                    'Hi I\u2019m Maya Gao,',
+                  ),
+                  __jsx(
+                    'div',
+                    {
+                      className: 'header',
+                      __source: {
+                        fileName: _jsxFileName,
+                        lineNumber: 39,
+                      },
+                      __self: this,
+                    },
+                    'I design tools to help people ',
+                    __jsx(
+                      'span',
+                      {
+                        className: '',
+                        __source: {
+                          fileName: _jsxFileName,
+                          lineNumber: 40,
+                        },
+                        __self: this,
+                      },
+                      'think, create, and collaborate',
+                    ),
+                  ),
+                ),
+                InternetLinks(),
+              ),
+              __jsx(
+                'section',
+                {
+                  id: 'second',
+                  className: 'column',
+                  __source: {
+                    fileName: _jsxFileName,
+                    lineNumber: 45,
+                  },
+                  __self: this,
+                },
+                __jsx(
+                  'p',
+                  {
+                    className: 'empahsis',
+                    __source: {
+                      fileName: _jsxFileName,
+                      lineNumber: 46,
+                    },
+                    __self: this,
+                  },
+                  "I'm passionate about making professional tools for technical audiences. I love to approach a project by learning a brand new subject, understanding its intricacies, and finding a balance between data and intuition.",
+                ),
+                __jsx(
+                  'p',
+                  {
+                    __source: {
+                      fileName: _jsxFileName,
+                      lineNumber: 51,
+                    },
+                    __self: this,
+                  },
+                  'In 2015, I joined',
+                  ' ',
+                  __jsx(
+                    'a',
+                    {
+                      target: '_blank',
+                      href: 'https://www.mapbox.com/',
+                      __source: {
+                        fileName: _jsxFileName,
+                        lineNumber: 53,
+                      },
+                      __self: this,
+                    },
+                    'Mapbox',
+                  ),
+                  ' ',
+                  'in Washington D.C. to help shape the first version of',
+                  ' ',
+                  __jsx(
+                    'a',
+                    {
+                      target: '_blank',
+                      href: 'https://www.wired.com/2015/09/mapbox-studio/',
+                      __source: {
+                        fileName: _jsxFileName,
+                        lineNumber: 57,
+                      },
+                      __self: this,
+                    },
+                    'Mapbox Studio',
+                  ),
+                  ', a custom map design tool for cartographers and developers. Cartography is a combination of art and science. Trying to dissect its complexities was a humbling experience.',
+                ),
+                __jsx(
+                  'p',
+                  {
+                    __source: {
+                      fileName: _jsxFileName,
+                      lineNumber: 63,
+                    },
+                    __self: this,
+                  },
+                  'I moved to',
+                  ' ',
+                  __jsx(
+                    'a',
+                    {
+                      href: 'https://stripe.com/',
+                      target: '_blank',
+                      __source: {
+                        fileName: _jsxFileName,
+                        lineNumber: 65,
+                      },
+                      __self: this,
+                    },
+                    'Stripe',
+                  ),
+                  ' ',
+                  'in 2017 where I led the design of Runkit, an interactive JavaScript prototyping playground in the browser. I contributed to a wide range of projects there, including designing better debugging and prototyping experiences for engineers.',
+                  ' ',
+                ),
+              ),
+              __jsx(
+                'div',
+                {
+                  id: 'third',
+                  className: 'column',
+                  __source: {
+                    fileName: _jsxFileName,
+                    lineNumber: 73,
+                  },
+                  __self: this,
+                },
+                __jsx(
+                  'p',
+                  {
+                    __source: {
+                      fileName: _jsxFileName,
+                      lineNumber: 74,
+                    },
+                    __self: this,
+                  },
+                  'After Stripe, I became the founding designer of',
+                  ' ',
+                  __jsx(
+                    'a',
+                    {
+                      href: 'https://observablehq.com/@mayagao',
+                      target: '_blank',
+                      __source: {
+                        fileName: _jsxFileName,
+                        lineNumber: 76,
+                      },
+                      __self: this,
+                    },
+                    'Observable',
+                  ),
+                  ' ',
+                  'in 2019, a visualization tool for data scientists. It was an exciting time at Observable where I got to define new systems for a product still in its nascent stage of development.',
+                ),
+                __jsx(
+                  'p',
+                  {
+                    __source: {
+                      fileName: _jsxFileName,
+                      lineNumber: 82,
+                    },
+                    __self: this,
+                  },
+                  "I'm currently leading design at ",
+                  __jsx(
+                    'a',
+                    {
+                      href: 'https://retool.com/',
+                      __source: {
+                        fileName: _jsxFileName,
+                        lineNumber: 83,
+                      },
+                      __self: this,
+                    },
+                    'Retool',
+                  ),
+                  '. We enable businesses of all sizes build internal tools and automate operational tasks. If problems like layout systems, permission controls, or configurable forms sound interesting to you,',
+                  ' ',
+                  __jsx(
+                    'a',
+                    {
+                      href: 'https://retool.com/careers/#product-designer',
+                      target: '_blank',
+                      __source: {
+                        fileName: _jsxFileName,
+                        lineNumber: 86,
+                      },
+                      __self: this,
+                    },
+                    'we are hiring',
+                  ),
+                  '.',
+                ),
+                InternetLinks(),
+              ),
+            ),
+          ),
+        )
+      }
+
+      /***/
+    },
+})
+//# sourceMappingURL=index.js.365868aa69720db9ab6a.hot-update.js.map
